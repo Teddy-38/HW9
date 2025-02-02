6a262b543ced580808d25ae1caf8c6bb04492085
Teddy-38 <Zhdanov_as94@mail.ru>
2025-02-02 21:56:57 +0800

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task1");
@@ -13,6 +11,15 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
-        System.out.println(sum);
-    }
-}
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        System.out.println("Task2");
+        int[] weekend = {1000, 2000, 3000, 4000, 5000};
+        int minAmount = weekend[0];
+        for (int i = 1; i < weekend.length; i++) {
+            if (weekend[i] < minAmount) {
+                minAmount = weekend[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minAmount + " рублей.");
+            }
+        }
