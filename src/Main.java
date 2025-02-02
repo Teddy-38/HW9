5a4451b9bf21167f628fd5c56b9e60a7a5fd8987
Teddy-38 <Zhdanov_as94@mail.ru>
2025-02-02 21:33:28 +0800

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Task1");
+        int[] arr = new int[5];
+        arr[0] = 15;
+        arr[1] = 25;
+        arr[2] = 35;
+        arr[3] = 45;
+        arr[4] = 55;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
     }
 }
